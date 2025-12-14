995bd0d9b0eb248d94bfe696bfa7d40bbfe9535d
ZIAD SAID <ziadsaid@ZIADs-MacBook-Pro.local>
2025-12-14 09:07:30 +0200

    fix: correct MongoDB connection variable name in code

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,19 @@ import { AuthModule } from './auth/auth.module';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/restaurantdb'), UsersModule, MenuModule, BookingsModule, OrdersModule, AuthModule, NotificationsModule],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 
+      process.env.MONGODB_URI || 
+      'mongodb://localhost:27017/restaurantdb'
+    ), 
+    UsersModule, 
+    MenuModule, 
+    BookingsModule, 
+    OrdersModule, 
+    AuthModule, 
+    NotificationsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
